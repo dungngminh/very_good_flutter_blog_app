15f81b7f5d99cd07cdfecf7fc657c87199652185
dungngminh <63831488+dungngminh@users.noreply.github.com>
2022-05-04 23:58:35 +0700

    chore: init very_good_blog_app

---
+++ android/app/src/main/kotlin/com/dungngminh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dungngminh.very_good_blog_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:very_good_blog_app/app/app.dart';
+import 'package:very_good_blog_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
