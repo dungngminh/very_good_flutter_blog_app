bb64b97c34e4e7002c5bf5908c773c0dc5b1c24e
thinh.le <roshan1234.lol@gmail.com>
2022-05-30 21:52:05 +0700

    feat(ssl): intilize route verifying certificate

--- backend/DjangoMongoSRL/config/urls.py
+++ backend/DjangoMongoSRL/config/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -24,6 +23,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_swagger.views import get_swagger_view
 from apps.swagger.views import TemplateView
+from django.views import static as another_static
 
 swagger_view = get_schema_view(
    openapi.Info(
@@ -40,7 +40,8 @@ swagger_view = get_schema_view(
 
 schema_view = get_swagger_view(title='My great API', url='./swagger.yaml')
 
-urlpatterns = [
+urlpatterns = [\
+    re_path(r'.well-known/pki-validation/(?P<path>.*)$', another_static.serve, {'document_root': settings.STATIC_ROOT + "/ssl-cert"}),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', swagger_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', swagger_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger-ui/', TemplateView.as_view()),
@@ -48,7 +49,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('apps.authentication.urls')),
     path('api/v1/', include('apps.users.urls')),
-    path('', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
