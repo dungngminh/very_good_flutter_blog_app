3d54f04bcae9f5195cae32b1cd7a90af8850ead5
mtoan65 <toandinh6501@outlook.com>
2022-06-25 19:52:24 +0700

    changed method get & added method post

--- backend/DjangoMongoSRL/apps/followings/views.py
+++ backend/DjangoMongoSRL/apps/followings/views.py
@@ -1,41 +1,83 @@
-from django.shortcuts import render
-from http.client import BAD_REQUEST
+import json
+from bson import json_util
+from django.core import serializers
+from bson import ObjectId
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from apps.utils.response import HttpResponse
-
-from apps.utils.jwt import JsonWebTokenHelper
-from .serializers import FollowingSerializer
-from apps.followings import serializers
-from rest_framework.response import Response
+from apps.utils.response import ResponseMessage
 from rest_framework import status
-from . import models
-from ..followings.models import Following
+from apps.utils.response import HttpResponse
+from apps.utils.jwt import JsonWebTokenHelper
+from apps.utils.database import mongo_extension
+from .models import Following
+from apps.users.models import User
+from apps.users.serializers import UserViewSerializer
 # Create your views here.
 
-class FollowingManage(APIView):
+def convert_objectId(x):
+    x['_id'] = str(x['_id'])
+    return x
+
+class FollowView(APIView):
     permission_classes = (AllowAny,)
-    serializer_class = FollowingSerializer
+    database = mongo_extension.get_database()
     
     def get(self, request, *args, **kwargs):
-        following_model = models.Following.objects
         try:
-            if "id_follow" in request.query_params:
-                following_model = following_model.filter(id=request.query_params["id_follow"])
-                
-            if "following" in request.query_params:
-                following_model = following_model.filter(content=request.query_params["following"])
-                
-            if "follower" in request.query_params:
-                following_model = following_model.filter(id_user=request.query_params["follower"])
-                 
-            serializer = FollowingSerializer(following_model, many = True)
+            user_id = request.query_params['user_id'] if "user_id" in request.query_params else ""
+            direction = request.query_params['direction'] if "direction" in request.query_params else ['in', 'out']
+
+            if direction == 'both':
+                direction = ['in', 'out']
+            else:
+                direction = [direction]
+
+            if not user_id:
+                return HttpResponse.response(
+                    data = {},
+                    message = ResponseMessage.INVALID_DATA,
+                    status = status.HTTP_400_BAD_REQUEST
+                )
             
-            return Response(serializer.data)
+            followers = []
+            followings = []
             
+            if 'in' in direction:
+                followers = list(self.database.followings_following.find({ 'sender': user_id }))
+                ids = list(map(lambda x: ObjectId(x['receiver']), followers))
+                followers = list(
+                    map(
+                        convert_objectId,
+                        list(self.database.users_user.find({ '_id': { '$in': ids } }, { 'password': 0 }))
+                    )
+                )
+
+            if 'out' in direction:
+                followings = list(self.database.followings_following.find({ 'sender': user_id }))
+                ids = list(map(lambda x: ObjectId(x['receiver']), followings))
+                followings = list(
+                    map(
+                        convert_objectId,
+                        list(self.database.users_user.find({ '_id': { '$in': ids } }, { 'password': 0 }))
+                    )
+                )
+
+            response_data = {}
+            if len(followers) > 0:
+                response_data['followers'] = followings
+            if len(followings) > 0:
+                response_data['followings'] = followings
+            
+            return HttpResponse.response(data=response_data, message=ResponseMessage.SUCCESS, status=status.HTTP_200_OK) 
+
         except Exception as e:
             print(e)
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return HttpResponse.response(
+                data = {},
+                message = ResponseMessage.INTERNAL_SERVER_ERROR,
+                status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
     
     def add(self, request):
         following_model = models.Following.objects
@@ -48,7 +90,58 @@ class FollowingManage(APIView):
         serializer = BlogSerializer(new_blog)
         return Response(serializer.data)
         
-    
+    def post(self, request, *args, **kwargs):
+        try:
+            jwt = request.META['HTTP_AUTHORIZATION']
+            # Author
+            payload = JsonWebTokenHelper.decode(jwt)
+            if not payload:
+                return HttpResponse.response(
+                    data = {},
+                    message = ResponseMessage.UNAUTHORIZED,
+                    status = status.HTTP_401_UNAUTHORIZED,
+                )
+            receiver_id = request.data['receiver_id']
+            sender_id = payload['_id']
+            # Check receiver, sender
+            receiver = self.database.users_user.find_one({ '_id': ObjectId(receiver_id) })
+            sender = self.database.users_user.find_one({ '_id': ObjectId(sender_id) })
+
+            if not sender or not receiver:
+                return HttpResponse.response(
+                    data = {},
+                    message = ResponseMessage.INVALID_DATA,
+                    status = status.HTTP_400_BAD_REQUEST
+                )
+
+            is_existed = self.database.followings_following.find_one({
+                "sender": sender_id,
+                "receiver": receiver_id,
+            })
+
+            if is_existed:
+                return HttpResponse.response(data={}, message='existed', status=status.HTTP_200_OK)
+
+            following_obj = Following.objects.create(
+                _id = ObjectId(),
+                sender = sender_id,
+                receiver = receiver_id,
+            )
+
+            following_obj.save()
+
+            return HttpResponse.response(
+                data = {},
+                message = ResponseMessage.SUCCESS,
+                status = status.HTTP_201_CREATED,
+            )
+            
+        except:
+            return HttpResponse.response(
+                data = {},
+                message = "missing_receiver",
+                status = status.HTTP_400_BAD_REQUEST,
+            )
     def patch(self, request, id=None):
         item = models.Following.objects.get(id = id)
         item.following = request.data["following"]
