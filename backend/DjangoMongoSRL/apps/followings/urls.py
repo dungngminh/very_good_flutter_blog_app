d7b463f13607fc891f4c197a9a6d85238455f44c
thanhnhat108 <thanhnhatpct108@gmail.com>
2022-06-26 14:41:12 +0700

    fix errors models Following

--- backend/DjangoMongoSRL/apps/followings/urls.py
+++ backend/DjangoMongoSRL/apps/followings/urls.py
@@ -2,12 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import FollowView
 
-# Follow
-# Unfollow
-
 router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('following', FollowView.as_view()),
     path('following/<str:id>', FollowView.as_view()),
+    
+]
 
