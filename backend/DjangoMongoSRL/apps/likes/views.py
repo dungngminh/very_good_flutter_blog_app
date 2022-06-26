4c900cb6f503a832ae4e2f0f3bd8a255bf64fbf2
thanhnhat108 <thanhnhatpct108@gmail.com>
2022-06-27 00:41:32 +0700

    change module like

--- backend/DjangoMongoSRL/apps/likes/views.py
+++ backend/DjangoMongoSRL/apps/likes/views.py
@@ -10,18 +10,24 @@ from apps.utils.database import mongo_extension
 from .models import Likes
 from datetime import datetime, timedelta
 
+# add code view like
+
 def convert_timestamp(data: datetime):
     local_time = (data + timedelta(hours=7))
     return int(datetime.timestamp(local_time))
 
 def convert_data(data):
     data['created_at'] = convert_timestamp(data['created_at'])
+    
     data['updated_at'] = convert_timestamp(data['updated_at'])
+    
     data['_id'] = str(data['_id'])
+    
     return data
 
 class LikesView(APIView):
     permission_classes = (AllowAny, )
+    
     database = mongo_extension.get_database()
 
     def post(self, request, *a, **b):
@@ -35,8 +41,11 @@ class LikesView(APIView):
 
             likes = Likes.objects.create(
                 _id = ObjectId(),
+                
                 blog_id = blog_id,
+                
                 user_id = user_id,
+                
             )
 
             likes.save()
@@ -72,7 +81,9 @@ class LikesView(APIView):
     def delete(self, request, *a, **b):
         try:
             payload = JsonWebTokenHelper.decode(request.META['HTTP_AUTHORIZATION'])
+            
             if not payload:
+                
                 return HttpResponse.response({}, ResponseMessage.UNAUTHORIZED, status.HTTP_401_UNAUTHORIZED)
             
             user_id = payload['_id']
@@ -86,7 +97,9 @@ class LikesView(APIView):
             return HttpResponse.response({}, ResponseMessage.SUCCESS, status.HTTP_200_OK)
 
         except Exception as e:
+            
             print(e)
+            
             return HttpResponse.response({}, ResponseMessage.INTERNAL_SERVER_ERROR, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
