532c14e44d1311f5ec0790633c9e40d1b720e69b
mtoan65 <toandinh6501@outlook.com>
2022-06-26 18:21:38 +0700

    rebuild bookmark api

--- backend/DjangoMongoSRL/apps/followings/urls.py
+++ backend/DjangoMongoSRL/apps/followings/urls.py
@@ -1,13 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import FollowView
-
-# Follow
-# Unfollow
+from . import views
 
 router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
-    path('following', FollowView.as_view()),
-    path('following/<str:id>', FollowView.as_view()),]
-
+    path('bookmarks', views.BookmarksView.as_view()),
+]
