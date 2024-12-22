94cad0245141352f5b7e2e822e1f65b9b2b06a60
dungngminh <63831488+dungngminh@users.noreply.github.com>
2024-12-22 11:22:45 +0700

    add docker compose

--- backend/main.dart
+++ backend/main.dart
@@ -3,13 +3,5 @@ import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
-  final flavor = Platform.environment['FLAVOR'];
-  if (flavor == 'DEV') {
-    return serve(handler, ip, port, poweredByHeader: null);
-  }
-  const customStaticDocumentPath = 'docs';
-  final cascade = Cascade()
-      .add(createStaticFileHandler(path: customStaticDocumentPath))
-      .add(handler);
-  return serve(cascade.handler, ip, port, poweredByHeader: null);
+  return serve(handler, ip, port, poweredByHeader: null);
 }
